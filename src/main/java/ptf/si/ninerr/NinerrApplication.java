43c7feaf30d9382296a43a06e19eeee9aebe567e
Nedim Bašić <nedim.basic.22@size.ba>
2024-11-23 00:10:53 +0100

    initCommit

---
+++ src/main/java/ptf/si/ninerr/NinerrApplication.java
@@ -0,0 +1,13 @@
+package ptf.si.ninerr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinerrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinerrApplication.class, args);
+	}
+
+}
